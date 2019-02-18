d9bc047d310bf74e7dd6a6690fc5d3f426afc785
ydhwang <ydhwang.ad@gmail.com>
2019-02-18 10:08:14 -0500

    added a part to check the returned value of rev-geocode.

--- R/sktmap.r
+++ R/sktmap.r
@@ -1,17 +1,19 @@
 #' R package for SK Telecom TMAP API
-#' geocode  \code{\link{sktmap}}
+#' geocode
 #'
 #' @author YD Hwang \email{yhwang@@g.skku.edu} and JK Kim \email{jkim@@iastate.edu}
 #' @param addr Target address character, which can be either old format (admin/lot number) or new format (road/building number)
 #' @param app_key SK Telecom OPEN API perosonal key from [TMAP webpage.](http://tmapapi.sktelecom.com/main.html#web/guide/webGuide.sample1)
 #' @return A tibble of 26 varialbles is returned. Its elements depend on how the address is provided into the function.
 #' @details
-#' * `newLat`, `newLon` is returned When *new address template is used (road - builidng number).
-#' * `lat`, `lon` is returned When *old* address template is used (admin-lot number)
+#' This function is *not* to be used for a lazy search; for a POI type search, change the code by yourself.
+#' * (`newLat`, `newLon`) is returned When *new* address template is used (road - builidng number).
+#' * (`lat`, `lon`) is returned When *old* address template is used (admin-lot number)
 #' @md
 #' @export
 
-tmap_geocode <- function(addr, app_key) {
+tmap_geocode <- function(addr = "서울시 종로구 성균관로 25-2", app_key) {
+  # a part for query format is needed here
   url <- paste0("https://api2.sktelecom.com/tmap/geo/fullAddrGeo?version=1&format=json&fullAddr=",
                 addr, "&appKey=", app_key)
   request <- GET(url, encode = "form")
@@ -21,7 +23,7 @@ tmap_geocode <- function(addr, app_key) {
 }
 
 
-#' reverse geocode function \code{\link{sktmap}}
+#' reverse geocode function
 #'
 #' @author YD Hwang \email{yhwang@@g.skku.edu} and JK Kim \email{jkim@@iastate.edu}
 #' @param lat Target latitude. Default value is for SKKU in Seoul, Korea.
@@ -51,13 +53,35 @@ tmap_rev_geocode <- function(lat = 37.587228, lon = 126.993115, app_key) {
   # 2: old address (admin-code or lot-number based)
   # 3: new address (road-centric)
   # fullAddress should be separated accordingly.
+  fullAddress <- response$fullAddress
+  address_vec <- str_split(fullAddress, pattern = ",", simplify = TRUE)
+
+  if (length(address_vec) == 3){
+    # when everything is nice
+    message("Address query seems fine.")
+  } else if (length(address_vec)==2){
+    if (str_count(response$roadName) == 0){
+      # no road name; third tuple is NA
+      address_vec_mod <- c(address_vec, "")
+      message("!!No road-address is found.")
+    } else if (str_count(response$legalDong) == 0){
+      # no legal dong name; the second tuple should be NA. Haven't seen such a case though
+      address_vec_mod <- c(address_vec[1], "", address_vec[2])
+      message("!!No legal-dong name is found.")
+    } else if (str_count(response$adminDong) == 0){
+      # no admin dong name; the first tuple should be NA. Haven't seen such a case though
+      address_vec_mod <- c("", address_vec)
+      message("!!No admin-dong name is found.")
+    } else {stop("Check your query again; found no matching address.")}
+    response$fullAddress <- paste0(address_vec_mod, collapse = ",")
+  } else  {stop("Check your lat/lon query again; found no matching address.")}
   out_tbl <- response %>%
     as_tibble %>%
     separate(fullAddress, sep =",", c("admin_name", "jibun_address", "road_address"))
   out_tbl
 }
 
-#' route search function \code{\link{sktmap}}
+#' route search function
 #'
 #' @author YD Hwang \email{yhwang@@g.skku.edu} and JK Kim \email{jkim@@iastate.edu}
 #' @param lat_origin origin's latitude.
@@ -65,7 +89,8 @@ tmap_rev_geocode <- function(lat = 37.587228, lon = 126.993115, app_key) {
 #' @param lat_dest destination latitude.
 #' @param lon_dest destination longitude.
 #' @param app_key SK Telecom OPEN API perosonal key from [TMAP webpage.](http://tmapapi.sktelecom.com/main.html#web/guide/webGuide.sample1).
-#' @return A Simple feature collection with 28 features and 16 fields. For more on simple feature in R, see sf package's manual.
+#' @return A Simple feature collection with 28 features and 16 fields. For more on simple feature in R, see [sf package.](https://cran.r-project.org/web/packages/sf/index.html)
+#' @md
 #' @export
 
 tmap_route_search <- function(lat_origin, lon_origin, lat_dest, lon_dest, app_key){
